fb10da61a27a75603bc097f1f22c335e526ab735
danny galimi <dannygalimi13@aim.com>
2021-03-07 02:45:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,9 +11,9 @@ erb :create_puppy
 end
 
 post '/puppy' do
-  #binding.pry
-  @puppy = Puppy.new(params[:name,:breed,:age])
   binding.pry
+  @puppy = Puppy.new(params[:name,:breed,:age])
+  #binding.pry
   erb :display_puppy
 end
 
